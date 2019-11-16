6b03bc9c8b67a0838a3c69d10e026b1ef1d34771
Sergiy tsimbalyuk <sv.tsimbalyuk@gmail.com>
2019-11-16 15:09:30 +0200

    reflect-config.json and pom got updated - should be no problem now with graalvm native image

--- build.sh
+++ build.sh
@@ -14,13 +14,13 @@ cd ./build/
 
 echo GRAAL native-image
 ${GRAAL}/bin/native-image -H:+ReportExceptionStackTraces \
-              --language:llvm \
+              -H:+TraceClassInitialization \
+              -H:ReflectionConfigurationFiles=../reflect-config.json \
               --no-server \
+              --language:llvm \
               --allow-incomplete-classpath \
               --class-path music-sorter.jar \
               --report-unsupported-elements-at-runtime \
-              --initialize-at-run-time=org.jaudiotagger.audio.asf.io.ChunkContainerReader,org.jaudiotagger.audio.asf.io.ContentDescriptionReader \
-              -H:ReflectionConfigurationFiles=../reflect-config.json \
               -H:Log=registerResource \
               -H:+ReportUnsupportedElementsAtRuntime \
               -H:+ReportExceptionStackTraces \
