c1c6a81cdb0cbb0cf340d63ab18be7302697ff36
Serhii Tsymbaliuk <sv.tsimbalyuk@gmail.com>
2022-10-29 20:47:32 +0300

    deprecation removed, sonar lint warnings about default constructor in record suppressed cuz reasons (:

--- src/main/java/dev/tsvinc/music/sort/domain/AppProperties.java
+++ src/main/java/dev/tsvinc/music/sort/domain/AppProperties.java
@@ -1,7 +1,8 @@
 package dev.tsvinc.music.sort.domain;
 
-import java.util.List;
 import lombok.Builder;
+
+import java.util.List;
 
 public record AppProperties(
         String sourceFolder,
@@ -10,6 +11,9 @@ public record AppProperties(
         boolean skipLiveReleases,
         List<String> liveReleasesPatterns) {
 
+    @SuppressWarnings({"java:S6207", "java:S1186"})
     @Builder
-    public AppProperties {}
+    public AppProperties {
+        // need this to make the builder work without loads of boilerplate
+    }
 }
