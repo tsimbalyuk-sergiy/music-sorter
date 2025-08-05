1de909a9a4dba7e202bacd7f56943929f1aa4ed7
Serhii Tsymbalyuk <sv.tsimbalyuk@gmail.com>
2025-08-05 11:59:33 +0300

    added checksumValidationEnabled property to configuration

--- src/main/java/dev/tsvinc/music/sort/domain/AppProperties.java
+++ src/main/java/dev/tsvinc/music/sort/domain/AppProperties.java
@@ -1,15 +1,42 @@
 package dev.tsvinc.music.sort.domain;
 
-import java.util.List;
-import lombok.Builder;
+import io.vavr.collection.List;
 
 public record AppProperties(
         String sourceFolder,
         String targetFolder,
         boolean sortByArtist,
         boolean skipLiveReleases,
-        List<String> liveReleasesPatterns) {
+        List<String> liveReleasesPatterns,
+        boolean checksumValidationEnabled) {
 
-    @Builder
-    public AppProperties {}
+    public AppProperties withSourceFolder(String sourceFolder) {
+        return new AppProperties(
+                sourceFolder,
+                targetFolder,
+                sortByArtist,
+                skipLiveReleases,
+                liveReleasesPatterns,
+                checksumValidationEnabled);
+    }
+
+    public AppProperties withTargetFolder(String targetFolder) {
+        return new AppProperties(
+                sourceFolder,
+                targetFolder,
+                sortByArtist,
+                skipLiveReleases,
+                liveReleasesPatterns,
+                checksumValidationEnabled);
+    }
+
+    public AppProperties withSortByArtist(boolean sortByArtist) {
+        return new AppProperties(
+                sourceFolder,
+                targetFolder,
+                sortByArtist,
+                skipLiveReleases,
+                liveReleasesPatterns,
+                checksumValidationEnabled);
+    }
 }
